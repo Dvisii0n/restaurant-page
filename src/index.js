60991956572f66a0cd2601c37169d81b9e2cfb89
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2025-07-15 21:20:02 -0600

    Added function to render page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import "./styles.css"
+import { home } from "./components/home.js";
 
 console.log("server running...")
+
+const content = document.querySelector("#content");
+
+function renderPage(page) {
+    content.innerHTML = "";
+    content.appendChild(page);
+    
+}
+
+renderPage(home());
